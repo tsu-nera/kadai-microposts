5e26accbd7092ac5d9ef516ccb8b776cd07d3a55
Tsunemichi Harada <fox10225fox@gmail.com>
2018-06-20 15:46:06 +0900

    finish 9

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
-  def gravatar_url(user, option = { size:80})
+  def gravatar_url(user, options = { size: 80 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    size = option[:size]
-    "https://secure.gravater.com/avater/#{gravatar_id}?s=#{size}"
+    size = options[:size]
+    "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
   end
 end
