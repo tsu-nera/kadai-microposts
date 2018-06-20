f556315861d4223f08e1e0111db1e3d5a837b5cf
tsu-nera <fox10225fox@gmail.com>
2018-06-20 22:35:34 +0900

    add relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save { self.email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
             format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
@@ -6,4 +7,8 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :microposts
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverses_of_relationship, source: :user
 end
